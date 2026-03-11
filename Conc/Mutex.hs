e802c660a60c3d26e0d1ecfcf6373844715ad0de
kINo <kinobhu@outlook.com>
2026-03-12 03:31:25 +0800

    Add `Mutex` library with its example

---
+++ Conc/Mutex.hs
@@ -0,0 +1,67 @@
+module Conc.Mutex
+  (
+    Mutex (..),
+    mutex,
+    motion,
+    mutexLock,
+    mutexUnlock,
+  )
+where
+
+import Utils.Queue qualified as Q
+import Core.Program
+import Conc.Spinlock
+
+data Mutex
+  = Mutex
+  { lock :: Int,
+    held :: Int,
+    queue :: Q.Queue
+  }
+
+mutex :: Int -> Int -> Int -> Mutex
+mutex addr qbeg qsize =
+  Mutex
+    addr
+    (addr + 1)
+    (Q.queue (addr + 2) qbeg qsize)
+
+motion :: Mutex -> [(Int, Int)]
+motion m = Q.motion $ queue m
+
+mutexLock :: Mutex -> Int -> Int -> Program
+mutexLock m t0 t1 = procedure $ do
+  spinLockYld (lock m) t0
+
+  spinTryLock (held m) t0
+  btr t0 $ Msg "mtx_lock_slow"
+
+  spinUnlock (lock m) t0
+  br $ Msg "mtx_lock_end"
+
+  lab "mtx_lock_slow"
+  tid t0
+  Q.enqueue (queue m) t0 t1
+  spinUnlock (lock m) t0
+  blk
+
+  lab "mtx_lock_end"
+
+mutexUnlock :: Mutex -> Int -> Int -> Program
+mutexUnlock m t0 t1 = procedure $ do
+  spinLockYld (lock m) t0
+
+  Q.qlength (queue m) t0 t1
+  bfs t0 $ Msg "mtx_unlock_empty"
+
+  Q.dequeue (queue m) t0 t1
+  spinUnlock (lock m) t1
+  pst t0
+  br $ Msg "mtx_unlock_end"
+
+  lab "mtx_unlock_empty"
+  imm t0 0
+  sto t0 (held m)
+  spinUnlock (lock m) t0
+
+  lab "mtx_unlock_end"
---
+++ examples/mutex.hs
@@ -0,0 +1,28 @@
+import Conc.Mutex
+import Core.Program
+
+mtx = mutex 0 10 10
+
+p1 = program $ do
+  prs "P1: enters."
+
+  mutexLock mtx 2 3
+  imm 1 10
+  sbi 1 1
+  prs "P1: occupying the lock ..."
+  btr 1 $ Num (-2)
+  mutexUnlock mtx 2 3
+
+p2 = program $ do
+  prs "P2: enters."
+  mutexLock mtx 0 1
+  prs "P2: I acquire the lock again!"
+  mutexUnlock mtx 0 1
+
+mem =
+  memory 50
+    // motion mtx
+
+main = do
+  let logs = schedule False 10 10 mem [p1, p2]
+  mapM_ putStrLn logs
