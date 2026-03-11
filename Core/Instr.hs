43da3485c1bf36a9694f5490b6ce32982717a2ea
kINo <kinobhu@outlook.com>
2026-03-12 01:28:09 +0800

    Refactor execution DSL format
    Using PC operation as the end.

--- Core/Instr.hs
+++ Core/Instr.hs
@@ -1,4 +1,3 @@
-{- HLINT ignore "Redundant pure" -}
 module Core.Instr
   ( Instr (..),
     Operator (..),
@@ -33,66 +32,55 @@ data Instr = Instr Operator Operand Operand
   deriving (Show)
 
 runInstr :: Instr -> Int -> Frame -> Execution Frame
-runInstr (Instr op (Num a) (Num b)) i f =
+runInstr (Instr op (Num a) (Num b)) tid f =
   case op of
     Add -> do
       x <- readRegM a f
       y <- readRegM b f
       f <- writeRegM a (x + y) f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Sub -> do
       x <- readRegM a f
       y <- readRegM b f
       f <- writeRegM a (x - y) f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Imm -> do
       f <- writeRegM a b f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Adi -> do
       x <- readRegM a f
       f <- writeRegM a (x + b) f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Sbi -> do
       x <- readRegM a f
       f <- writeRegM a (x - b) f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Lod -> do
       m <- readMem b
       f <- writeRegM a m f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Sto -> do
       x <- readRegM a f
       writeMem b x
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Ldr -> do
       ma <- readRegM b f
       m <- readMem ma
       f <- writeRegM a m f
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Str -> do
       ma <- readRegM b f
       x <- readRegM a f
       writeMem ma x
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Cas -> do
       m <- readMem b
       f <- writeRegM a m f
       when (m == 0) $
         writeMem b 1
-      f <- mapPcM (+ 1) f
-      pure f
+      mapPcM (+ 1) f
     Br -> do
-      f <- mapPcM (+ a) f
-      pure f
+      mapPcM (+ a) f
     Btr -> do
       x <- readRegM b f
       let dpc = if x /= 0 then a else 1
@@ -103,24 +91,23 @@ runInstr (Instr op (Num a) (Num b)) i f =
        in mapPcM (+ dpc) f
     _ -> mapPcM (+ 1) f
 
-runInstr (Instr op (Msg a) (Num b)) i f =
+runInstr (Instr op (Msg a) (Num b)) tid f =
   case op of
     Br -> do
-      d <- findsym i a
-      f <- setPcM d f
-      pure f
+      d <- findsym tid a
+      setPcM d f
     Btr -> do
       x <- readRegM b f
       if x /= 0
         then do
-          d <- findsym i a
+          d <- findsym tid a
           setPcM d f
         else mapPcM (+ 1) f
     Bfs -> do
       x <- readRegM b f
       if x == 0
         then do
-          d <- findsym i a
+          d <- findsym tid a
           setPcM d f
         else mapPcM (+ 1) f
     _ -> mapPcM (+ 1) f
