4600d245b1e3c4cfa7a7254694242c2df19740fe
kINo <kinobhu@outlook.com>
2026-03-12 03:31:07 +0800

    Complete `Spinlock.hs`

--- Conc/Spinlock.hs
+++ Conc/Spinlock.hs
@@ -1,18 +1,27 @@
 module Conc.Spinlock
   ( spinLock,
     spinUnlock,
+    spinTryLock,
+    spinLockYld
   )
 where
 
 import Core.Instr
 import Core.Program
 
-spinLock :: Int -> Int -> Program
 spinLock lock t = procedure $ do
   cas t lock
   btr t $ Num (-1)
 
-spinUnlock :: Int -> Int -> Program
+spinTryLock lock t = procedure $ do
+  cas t lock
+
 spinUnlock lock t = procedure $ do
   imm t 0
   sto t lock
+
+spinLockYld lock t = procedure $ do
+  cas t lock
+  bfs t (Num 3)
+  yld
+  br (Num (-3))
