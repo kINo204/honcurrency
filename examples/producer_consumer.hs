3aa4db01aa03eba46f4a202aa9233f09163a8ff7
kINo <kinobhu@outlook.com>
2026-03-13 00:25:51 +0800

    Add example producer consumer

---
+++ examples/producer_consumer.hs
@@ -0,0 +1,41 @@
+import Conc.Mutex as M
+import Conc.Semaphore as S
+import Core.Program
+
+numObjects = semaphore 0 0 100 10 -- 0 objects at start
+
+numBlanks = semaphore 2 10 110 10 -- 2 buffers left
+
+mtx = mutex 20 120 10
+
+producer i = program $ do
+  semWait numBlanks 0 1
+  mutexLock mtx 0 1
+
+  prs $ "--> Producer " ++ show i
+
+  mutexUnlock mtx 0 1
+  semPost numObjects 0 1
+
+consumer i = program $ do
+  semWait numObjects 0 1
+  mutexLock mtx 0 1
+
+  prs $ "<-- Consumer " ++ show i
+
+  mutexUnlock mtx 0 1
+  semPost numBlanks 0 1
+
+mem =
+  memory 200
+    // M.motion mtx
+    // S.motion numObjects
+    // S.motion numBlanks
+
+main = do
+  let m = 5
+  let n = 7
+  let producers = [producer i | i <- [0 .. m]]
+  let consumers = [consumer i | i <- [0 .. n]]
+  let logs = schedule False 5 10 mem (producers ++ consumers)
+  mapM_ putStrLn logs
